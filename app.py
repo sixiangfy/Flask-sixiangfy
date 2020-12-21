3f71db978d3ee24110dc30a5a26efb821fea4a56
sixiangfy <sixiangfy@163.com>
2020-12-21 16:34:23 +0800

    依赖

--- app.py
+++ app.py
@@ -7,15 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
-@app.route('/user/<name>',methods=['get','post'])
-def user(name):
-    return name
-
-
-@app.route('/nihao')
-def nihao():
-    return
 
 
 if __name__ == '__main__':
     app.run(FLASK_DEBUG=1)
+
+
